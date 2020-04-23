5314eec95229a139de108b1d3c7c90d523246320
inavka <ivanka.zhuravchak@gmail.com>
2020-04-23 15:03:23 +0200

    Add project files.

---
+++ Brueckenkurs_1/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+
+namespace Brueckenkurs_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // //Convertierung von int to double
+           // int testInteger;
+           // double testDouble;
+
+           // testInteger = 22;
+           // testDouble = Convert.ToDouble(testInteger);
+
+
+           // //Aufgabe 2, 3, 4//
+
+           // Console.WriteLine("Becker");
+           // Console.WriteLine("Ivanka #1");
+
+           // //oder
+
+           // Console.WriteLine("Becker \nIvannka #2");
+
+           // Console.WriteLine("Ivanka " + "Becker");
+
+           ////Aufgabe 5, 6
+
+           // string eingabe;
+
+           // eingabe = Console.ReadLine();
+
+           // Console.WriteLine(eingabe);
+
+           // Console.WriteLine("Das ist mein Text: " + eingabe);
+
+           // //Aufgabe 7
+
+           // Console.WriteLine("Wie heißt du?");
+           // string name = Console.ReadLine();
+
+           // Console.WriteLine("Wie alt bist du?");
+           // string alter = Console.ReadLine();
+
+           // Console.WriteLine("Ich bin " + name + " und " + alter + " Jahre alt.");
+           // Console.WriteLine($"Mein Name ist {name} und ich bin {alter} Jahre alt.");
+           // //Aufgabe 8
+
+           // int a = 5 + 34;
+           // int b = 4 * 38;
+           // int c = 60 / 20;
+
+           // Console.WriteLine("Ergebnis: " + a + ", " + b + ", " + c);
+
+           // //Aufgabe 9
+
+           // a = 5 + 5 * 2;
+           // b = (5 + 5) * 2;
+           // c = 6 / 2 + 5;
+
+           // Console.WriteLine("Ergebnis: " + a + ", " + b + ", " + c);
+
+           // //Aufgabe 10
+
+           // int test = 10;
+           // Console.WriteLine("int Variable: " + test);
+
+           // //Aufgabe 11
+
+           // int summe = 8 + 22;
+           // Console.WriteLine("Summe von ints " + summe);
+
+           // //Aufgabe 12
+
+           // a = 8;
+           // b = 22;
+           // c = a + b;
+
+           // Console.WriteLine("Neue Variable: " + c);
+
+           // //Aufgabe 13
+
+           // double e = 3.67;
+           // e = e * 5;
+
+           // Console.WriteLine("double Variable: " + e);
+
+           // //Aufgabe 14
+
+           // double meinWert1 = 6;
+           // int meinWert2 = 4;
+           // double ergebnis = meinWert1 / meinWert2;
+
+           // Console.WriteLine("Neue double Variable: " + ergebnis);
+
+           // //Aufgabe 15
+
+           // string wertText = "123";
+           // int wertZahl;
+           // wertZahl = Int32.Parse(wertText);
+           // int testSumme = wertZahl + wertZahl;
+           // Console.WriteLine("Test für Umwandlung: " + testSumme);
+
+           // //Aufgabe 16
+
+           // Console.WriteLine("In welchem Jahr bist du geboren?");
+           // int jahr = Int32.Parse(Console.ReadLine());
+           // int alter1 = 2019 - jahr;
+           // Console.WriteLine($"Du bist {alter1} Jahre alt");
+
+           // //Aufgabe 17
+           // Console.Write("Temperatur in Celsius: ");
+           // double celsius = Convert.ToDouble(Console.ReadLine());
+
+           // double fahrenheit = celsius * 1.8 + 32;
+
+           // Console.WriteLine($"Temperatur in Fahrenheit: {fahrenheit}");
+
+           // //Aufgabe 18 
+           // Console.Write("Betrag in Euro: ");
+           // double euro = Convert.ToDouble(Console.ReadLine());
+
+           // double dollar = euro * 1.18;
+
+           // Console.WriteLine($"Betrag in Dollar: {dollar:f2}");
+
+           // //Aufgabe 19
+
+           // Console.WriteLine("Wie alt bist du?");
+           // int alter3 = Convert.ToInt32(Console.ReadLine());
+
+           // if (alter3>=18)
+           // {
+           //     Console.WriteLine("volljärig");
+           // } else
+           // {
+           //     Console.WriteLine("minderjährig");
+           // }
+
+           // //Aufgabe 20 
+           // Console.WriteLine("In welchem Jahr bist du geboren?");
+           // int jahr1 = Convert.ToInt32(Console.ReadLine());
+
+           // alter3 = 2019 - jahr1;
+
+           // if (alter3 >= 18)
+           // {
+           //     Console.WriteLine("volljärig");
+           // }
+           // else
+           // {
+           //     Console.WriteLine("minderjährig");
+           // }
+
+           // //Aufgabe 21
+           // Console.WriteLine("Wie viele Tage hat der September?");
+           // int antwort = Convert.ToInt32(Console.ReadLine());
+
+           // if (antwort == 31) 
+           // {
+           //     Console.WriteLine("richtig");
+           // } 
+           // else
+           // {
+           //     Console.WriteLine("falsch");
+           // }
+
+           // //Aufgabe 22
+           // Console.Write("Zahl 1: ");
+           // int z1 = Convert.ToInt32(Console.ReadLine());
+
+           // Console.Write("Zahl 2: ");
+           // int z2 = Convert.ToInt32(Console.ReadLine());
+
+           // if ((z1+z2) > 20)
+           // {
+           //     Console.WriteLine("Groesser als 20");
+
+           // } else if ((z1+z2) < 20)
+           // {
+           //     Console.WriteLine("Kleiner als 20");
+           // } else
+           // {
+           //     Console.WriteLine("Gleich");
+           // }
+
+           // //Aufgabe 23
+           // Console.Write("Preis: ");
+           // double preis = Convert.ToDouble(Console.ReadLine());
+
+           // Console.Write("Anzahl: ");
+           // double anzahl = Convert.ToDouble(Console.ReadLine());
+
+           // double gesamt = preis * anzahl;
+
+           // Console.WriteLine("Geldbetrag: ");
+           // double betrag = Convert.ToDouble(Console.ReadLine());
+
+           // if (betrag>gesamt)
+           // {
+           //     Console.WriteLine("Rueckgeld: " + (betrag - gesamt));
+
+           // } else if (betrag<gesamt)
+           // {
+           //     Console.WriteLine("Noch zu zahlen: " + (-1 * (betrag - gesamt)) );
+           // } else
+           // {
+           //     Console.WriteLine("Vielen Dank für Ihren Einkauf!");
+           // }
+
+            //Aufgabe 24
+
+        }
+    }
+}
